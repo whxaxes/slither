35f58794d70886d2f0729930941087c49936aa26
wanghx <whx89768@alibaba-inc.com>
2016-04-26 10:09:58 +0800

    update

--- src/Map.js
+++ src/Map.js
@@ -6,6 +6,7 @@
  */
 'use strict';
 
+// 地图类
 export default class Map {
 
   constructor(options) {
@@ -14,37 +15,60 @@ export default class Map {
     this.height = options.height;
 
     // 背景块的大小
-    this.block_w = 100;
-    this.block_h = 100;
+    this.block_w = 150;
+    this.block_h = 150;
 
-    // 视窗位置
-    this.frame_w = options.canvas.width;
-    this.frame_h = options.canvas.height;
-    this.frame_x = options.frame_x || 0;
-    this.frame_y = options.frame_y || 0;
+    // 视窗
+    this.frame = new Frame({
+      w: options.canvas.width,
+      h: options.canvas.height,
+      x: options.frame_x,
+      y: options.frame_y
+    });
   }
 
   render() {
-    const begin_x = this.block_w - this.frame_x % this.block_w;
-    const begin_y = this.block_h - this.frame_y % this.block_h;
-    const end_x = (this.frame_x + this.frame_w) - (this.frame_x + this.frame_w) % this.block_w;
-    const end_y = (this.frame_y + this.frame_h) - (this.frame_y + this.frame_h) % this.block_h;
+    const frame = this.frame;
+    const begin_x = this.block_w - frame.x % this.block_w;
+    const begin_y = this.block_h - frame.y % this.block_h;
+    const end_x = (frame.x + frame.w) - (frame.x + frame.w) % this.block_w;
+    const end_y = (frame.y + frame.h) - (frame.y + frame.h) % this.block_h;
 
     this.ctx.save();
     this.ctx.beginPath();
-    this.ctx.strokeStyle = '#999';
+    this.ctx.strokeStyle = '#aaa';
 
-    for(let x = begin_x; x <= end_x; x += this.block_w) {
+    for (let x = begin_x; x <= end_x; x += this.block_w) {
       this.ctx.moveTo(x, 0);
-      this.ctx.lineTo(x, this.frame_h);
+      this.ctx.lineTo(x, frame.h);
     }
 
-    for(let y = begin_y; y <= end_y; y += this.block_w) {
+    for (let y = begin_y; y <= end_y; y += this.block_w) {
       this.ctx.moveTo(0, y);
-      this.ctx.lineTo(this.frame_w, y);
+      this.ctx.lineTo(frame.w, y);
     }
 
     this.ctx.stroke();
     this.ctx.restore();
+  }
+}
+
+// 视窗类
+class Frame {
+  constructor(options) {
+    this.w = options.w;
+    this.h = options.h;
+    this.x = options.x;
+    this.y = options.y;
+  }
+
+  /**
+   * 移动视窗
+   * @param x
+   * @param y
+   */
+  translate(x, y) {
+    this.x += x;
+    this.y += y;
   }
 }
